0ac76a396661f8211c39e9d49ce511ddf36f4f2c
ocefpaf@gmail.com <ocefpaf@gmail.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2011-06-03 19:47:50 +0000

    Added GUI directory, just a place holder for now.
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/branches/2011-05-18@778 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

---
+++ gsoc/gui/hello.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+"""Hello, wxPython! program."""
+
+import wx
+
+class Frame(wx.Frame):
+    """Frame class that display an image."""
+
+    #pass
+    def __init__(self, image, parent=None, id=-1,
+                 pos=wx.DefaultPosition,
+                 title='Hello, wxPython!'):
+        """Create a Frame instance and display image."""
+        temp = image.ConvertToBitmap()
+        size = temp.GetWidth(), temp.GetHeight()
+        wx.Frame.__init__(self, parent, id, title, pos, size)
+        self.bmp = wx.StaticBitmap(parent=self, bitmap=temp)
+
+class App(wx.App):
+    """Application class."""
+
+    def OnInit(self):
+        image = wx.Image('header.jpg', wx.BITMAP_TYPE_JPEG)
+        self.frame = Frame(image)
+        #self.frame = wx.Frame(parent=None, title='Spare')
+        self.frame.Show()
+        self.SetTopWindow(self.frame)
+        return True
+
+def main():
+    app = App()
+    app.MainLoop()
+
+if __name__ == '__main__':
+    main()
