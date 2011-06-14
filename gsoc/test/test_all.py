c5f884a8f0ec0bf96c66bc3117c6a182ba18566a
ocefpaf@gmail.com <ocefpaf@gmail.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2011-06-14 17:48:58 +0000

    Place for unittest directory.
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/branches/2011-05-18@790 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

---
+++ gsoc/test/test_all.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# $URL$
+# $Rev$
+#
+# test_all.py
+#
+# Filipe Fernandes, 2011-06-13
+
+"""Perform all unit tests in the present directory"""
+
+import sys
+import os
+import glob
+
+# Set executables for python2 and python3
+python2 = 'python'   # python 2.x
+python3 = 'python3'  # python 3.x
+
+# Choose the correct executable
+if sys.version[0] == 3:   # Version 3.x
+    python = python3
+else:
+    python = python2
+
+# Find all test scripts
+test_scripts = glob.glob('test*.py')
+test_scripts.remove('test_all.py')  # test_all.py is not a unit-test
+
+# Perform the unit-tests and accumulate errors
+errors = []
+for script in test_scripts:
+    sys.stdout.write('running %s\n' % script)
+    a = os.system('%s %s' % (python, script))
+    if a != 0:
+        errors.append(script)
+
+# Display a summary
+sys.stdout.write("\n --- test_all.py finished ---\n")
+sys.stdout.write("  Ran %d test scripts\n" % len(test_scripts))
+if not errors:
+    sys.stdout.write("  no problems\n")
+else:
+    sys.stdout.write("  problems in :\n")
+    sys.stdout.write(str(errors))
