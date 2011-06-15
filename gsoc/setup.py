e0715d23b6de22490062783199073a2b1225159e
ocefpaf@gmail.com <ocefpaf@gmail.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2011-06-15 21:52:22 +0000

    Unified setup.py for all platforms.
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/branches/2011-05-18@797 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

--- gsoc/setup.py
+++ gsoc/setup.py
@@ -6,6 +6,7 @@
 #
 # Filipe Fernandes, 2011-05-29
 
+import sys
 from distutils.core import setup
 from distutils.command.sdist import sdist
 
@@ -13,6 +14,9 @@ try: # Python 3
   from distutils.command.build_py import build_py_2to3 as build_py
 except ImportError: # Python 2
   from distutils.command.build_py import build_py
+
+mainscript = 'CCCgistemp/tool/ccc-gistemp.py'
+data_files = [('',['readme.txt', 'LICENSE.txt', 'release-notes.txt'])]
 
 classifiers = """\
 Development Status :: 5 - Production/Stable
@@ -28,21 +32,46 @@ Topic :: Education
 Topic :: Software Development :: Libraries :: Python Modules
 """
 
+if sys.platform == 'darwin':
+    import py2app
+    extra_options = dict(
+        app=[mainscript],
+        options=dict(py2app=dict(argv_emulation=True)),
+        )
+elif sys.platform == 'win32':
+    import py2exe
+    extra_options = dict(
+        console=[{"script": mainscript,
+        "icon_resources": [(1, "ccf.ico")]}],
+        options = {"py2exe": {
+            "compressed":1,
+            "optimize":2,
+            "bundle_files":2,
+            "dist_dir":'dist',
+            "xref":False,
+            "skip_archive":False,
+            "ascii": False,
+            "custom_boot_script":''
+            }},
+        )
+else:
+    extra_options = dict(
+        scripts=[mainscript],
+        )
 
 setup(name = 'ccc-gistemp',
-      version = '0.6.1',
-      packages = ['CCCgistemp','CCCgistemp.code','CCCgistemp.tool'],
-      license = 'LICENSE.txt',
-      description = """ccc-gistemp is a reimplementation of GISTEMP in Python""",
-      long_description = open('readme.txt').read(), # change to capitals
-      author = 'Nick Barnes, David Jones',
-      author_email = 'ccc-gistemp@climatecode.org',
-      url = 'http://code.google.com/p/ccc-gistemp/',
-      download_url =
-      'http://ccc-gistemp.googlecode.com/files/ccc-gistemp-0.6.1.tar.gz',
-      classifiers = filter(None, classifiers.split("\n")),
-      platforms = 'any',
-      cmdclass = {'build_py': build_py},
-      keywords = ['science', 'climate', 'GIS', 'temperature'],
-      scripts=['CCCgistemp/tool/ccc-gistemp.py'],
+      version='0.6.1',
+      packages=['CCCgistemp','CCCgistemp.code','CCCgistemp.tool'],
+      license='LICENSE.txt',
+      description="""ccc-gistemp is a reimplementation of GISTEMP in Python""",
+      long_description=open('readme.txt').read(), # change to capitals
+      author='Nick Barnes, David Jones',
+      author_email='ccc-gistemp@climatecode.org',
+      url='http://code.google.com/p/ccc-gistemp/',
+      download_url='http://ccc-gistemp.googlecode.com/files/ccc-gistemp-0.6.1.tar.gz',
+      classifiers=filter(None, classifiers.split("\n")),
+      platforms='any',
+      cmdclass={'build_py': build_py},
+      keywords=['science', 'climate', 'GIS', 'temperature'],
+      **extra_options
      )
