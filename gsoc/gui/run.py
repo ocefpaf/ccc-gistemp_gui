335d6630931381794c4566e7a199b30e903d3e9a
ocefpaf@gmail.com <ocefpaf@gmail.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2011-06-30 00:36:35 +0000

    Added close button and text frame for stdout and stderr.
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/branches/2011-05-18@806 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

--- gsoc/gui/run.py
+++ gsoc/gui/run.py
@@ -8,32 +8,76 @@
 
 import wx
 import sys
-from CCCgistemp.tool import run
 
-class InsertFrame(wx.Frame):
+class Frame(wx.Frame):
 
     def __init__(self, parent, id):
-        wx.Frame.__init__(self, parent, id, 'run button',
-                size=(300, 100))
+        wx.Frame.__init__(self, parent, id, 'ccc-gistemp',
+                size=(600, 400))
+        self.Bind(wx.EVT_CLOSE, self.OnClose)
+        menuBar = wx.MenuBar()
+        menu = wx.Menu()
+        m_exit = menu.Append(wx.ID_EXIT, "E&xit\tAlt-X", "Close window and exit program.")
+        self.Bind(wx.EVT_MENU, self.OnClose, m_exit)
+        menuBar.Append(menu, "&File")
+        self.statusbar = self.CreateStatusBar()
 
+        # panel
         panel = wx.Panel(self)
-        button = wx.Button(panel, label="run", pos=(125, 10), size=(50, 50))
-        self.Bind(wx.EVT_BUTTON, self.RunCCCgistemp, button)
-        self.Bind(wx.EVT_CLOSE, self.OnCloseWindow)
+        box = wx.BoxSizer(wx.VERTICAL)
+        m_text = wx.StaticText(panel, -1, "ccc-gistemp")
+        m_text.SetFont(wx.Font(14, wx.SWISS, wx.NORMAL, wx.BOLD))
+        m_text.SetSize(m_text.GetBestSize())
+        box.Add(m_text, 0, wx.ALL, 10)
+
+        log = wx.TextCtrl(panel, wx.ID_ANY, size=(400,400), pos=(150,0),
+                          style = wx.TE_MULTILINE|wx.TE_READONLY|wx.HSCROLL)
+
+        # redirect text here
+        redir=RedirectText(log)
+        sys.stderr=redir
+        #sys.stdout=redir
+
+        # run button
+        run_button = wx.Button(panel, label="Run!")
+        run_button.Bind(wx.EVT_BUTTON, self.RunCCCgistemp)
+        box.Add(run_button, 0, wx.ALL, 10)
+
+        # close button
+        close_button = wx.Button(panel, wx.ID_CLOSE, label="Exit")
+        close_button.Bind(wx.EVT_BUTTON, self.OnClose)
+        box.Add(close_button, 0, wx.ALL, 10)
+
+        panel.SetSizer(box)
+        panel.Layout()
+
+    def OnClose(self, event):
+        dlg = wx.MessageDialog(self, "Do you want to close the application?",
+                            "Confirm Exit", wx.OK|wx.CANCEL|wx.ICON_QUESTION)
+        result = dlg.ShowModal()
+        dlg.Destroy()
+        if result == wx.ID_OK:
+            self.Destroy()
 
     def RunCCCgistemp(self, event):
-        print "running ccc-gistemp.py" # need to be able to import and then run!
-                                       # the current way requires a separated
-                                       # CCCgistemp installation.
+        from CCCgistemp.tool import run
+        print "running ccc-gistemp.py"
         run.main()
-        self.Destroy()
+        #self.Destroy() TODO: some message
 
     def OnCloseWindow(self, event):
         print "Done!"
         self.Destroy()
 
+class RedirectText(object):
+    def __init__(self, aWxTextCtrl):
+        self.out = aWxTextCtrl
+
+    def write(self, string):
+        self.out.WriteText(string)
+
 if __name__ == '__main__':
-    app = wx.PySimpleApp()
-    frame = InsertFrame(parent=None, id=-1)
+    app = wx.App(redirect=False)
+    frame = Frame(parent=None, id=-1)
     frame.Show()
     app.MainLoop()
