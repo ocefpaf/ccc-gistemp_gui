935401a66a7ed51a8abc5fbb7362a644afe429a0
ocefpaf@gmail.com <ocefpaf@gmail.com@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2011-08-03 00:27:46 +0000

    Better displaying of tabular data in spreadsheet programs.
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/branches/2011-05-18@873 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

--- gsoc/gui/gistemp2csv.py
+++ gsoc/gui/gistemp2csv.py
@@ -6,30 +6,58 @@
 #
 # Filipe Fernandes, 2011-08-01
 
-import csv
+import os
 import re
+import csv
 
-def gistemp2csv(fin="mixedGLB.Ts.ho2.GHCN.CL.PA.txt",
-                fout="mixedGLB.Ts.ho2.GHCN.CL.PA.csv"):
+def gistemp2csv(fin):
     """
+    Write a comma separated value file from ccc-gistemp text output.
     """
-    csv_out = csv.writer(open(fout, 'wb'), delimiter=',')
+
+    basename = os.path.splitext(os.path.basename(fin))[0]
+
+    fout = open(basename + '.csv', 'wb')
+
+    csv_out = csv.writer(fout, delimiter=',')
     header = []
+    field = ''
 
     with open(fin) as lines:
         for line in lines:
-            if re.match("^[1-9]+.*", line):
-                csv_out.writerow(line.strip().split())
-            elif re.match("^Year+.*", line):
-                csv_out.writerow(line.strip().split())
-            else:
-                if line.strip():
-                    header += line
+            if re.match(r"^(18|19|20)\d{2}", line):
+                # Remove the last redundant year entry.
+                data = line.strip().split()[:-1]
+                # Write rows with years and temperatures anomalies
+                csv_out.writerow(data)
 
-    with open(fout, "r+") as f:
+                # TODO: Handle the asterisks.
+                if len(data) != length:
+                    print("Warning, this line has less entries than expected\n" + data)
+
+            elif re.match(r"^Year", line) and (not field):
+                # Write only first occurrence of the filed descriptor.
+                field = line.split()[:-1]
+                csv_out.writerow(field)
+                length = len(field)
+
+            elif line.strip() and (not field):
+                header += line
+
+    header = ''.join(header)
+    fout.close()
+
+    with open(basename + '.csv', "r+") as f:
         old = f.read()
         f.seek(0)
-        f.write(''.join(header) + '\n' + old)
+        f.write(header + '\n' + old)
 
 if __name__ == '__main__':
-    gistemp2csv()
+    import glob
+    files = glob.glob("*.txt")
+    # TODO: The Zonnal files have a different field.
+    #                            24N   24S   90S     64N   44N   24N   EQU   24S   44S   64S   90S
+    # Year  Glob  NHem  SHem    -90N  -24N  -24S    -90N  -64N  -44N  -24N  -EQU  -24S  -44S  -64S
+    # where 24N and -90N should be placed in the same cell (I guess!?).
+    for f in files:
+        gistemp2csv(f)
