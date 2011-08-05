ebfcf1486bb89aa94327038f2d6db1dfc5f08f25
ocefpaf <ocefpaf@a3c903a6-8ee9-11dd-8a4e-8714b3728bc5>
2011-08-05 16:52:41 +0000

    Added a chunks() func to deal with fixed size columns. Now the non-zonal avg is dealt separately from the zonal avg.
    git-svn-id: https://ccc-gistemp.googlecode.com/svn/branches/2011-05-18@889 a3c903a6-8ee9-11dd-8a4e-8714b3728bc5

--- gsoc/gui/gistemp2csv.py
+++ gsoc/gui/gistemp2csv.py
@@ -6,58 +6,87 @@
 #
 # Filipe Fernandes, 2011-08-01
 
+# http://docs.python.org/release/2.4.4/lib/module-os.path.html
 import os
+# http://docs.python.org/release/2.4.4/lib/module-re.html
 import re
+# http://docs.python.org/release/2.4.4/lib/module-csv.html
 import csv
 
+
+def chunks(l, n):
+    """
+    return n-sized chunks from  the list l.
+    """
+    return [l[i:i + n] for i in range(0, len(l), n)]
+
+
+def non_zonal(line):
+    """Break Non-Zonal file format into specified chunks."""
+
+    # The first 12 entries are 5 characters width columns.
+    month = chunks(line[:65], 5)
+    # After 3 white spaces it becomes 4 characters width columns.
+    year = chunks(line[68:76], 4)
+    # After 2 white spaces it is back to 5 characters width columns.
+    # the -5 removes the last redundant year entry.
+    season = chunks(line[78:-5], 5)
+    # Write rows with years and temperatures anomalies.
+    return month + year + season
+
+
 def gistemp2csv(fin):
-    """
-    Write a comma separated value file from ccc-gistemp text output.
-    """
+    """ Write a comma separated value file from ccc-gistemp text output."""
 
     basename = os.path.splitext(os.path.basename(fin))[0]
 
     fout = open(basename + '.csv', 'wb')
 
     csv_out = csv.writer(fout, delimiter=',')
+
     header = []
-    field = ''
+    field1 = ''
+    field2 = ''
 
     with open(fin) as lines:
         for line in lines:
+            # If start with with a number Year it is data.
             if re.match(r"^(18|19|20)\d{2}", line):
-                # Remove the last redundant year entry.
-                data = line.strip().split()[:-1]
-                # Write rows with years and temperatures anomalies
+                # Non-Zonal avg files are treated differently.
+                if not 'Zon' in basename:
+                    data = non_zonal(line)
+                else:
+                    data = line.split()[:-1]
                 csv_out.writerow(data)
 
-                # TODO: Handle the asterisks.
-                if len(data) != length:
-                    print("Warning, this line has less entries than expected\n" + data)
+            # Write only first occurrence of the field descriptor 1.
+            elif re.match(r"^Year", line) and (not field1):
+                field1 = line.split()[:-1]
+                csv_out.writerow(field1)
 
-            elif re.match(r"^Year", line) and (not field):
-                # Write only first occurrence of the filed descriptor.
-                field = line.split()[:-1]
-                csv_out.writerow(field)
-                length = len(field)
+            # Write only first occurrence of the field descriptor 2.
+            # only Zonal avg files have this second descriptor.
+            elif "90S" in line and (not field2):
+                field2 = [' '] * 5 + line.split()[:-1]
+                csv_out.writerow(field2)
 
-            elif line.strip() and (not field):
+            # This passes fields 1 and 2 and blank lines.
+            # Keeping all the rest as header.
+            elif line.strip() and (not field1) and (not field2):
                 header += line
 
     header = ''.join(header)
     fout.close()
 
+    # Write the header without the commas.
     with open(basename + '.csv', "r+") as f:
         old = f.read()
         f.seek(0)
         f.write(header + '\n' + old)
 
+
 if __name__ == '__main__':
     import glob
     files = glob.glob("*.txt")
-    # TODO: The Zonnal files have a different field.
-    #                            24N   24S   90S     64N   44N   24N   EQU   24S   44S   64S   90S
-    # Year  Glob  NHem  SHem    -90N  -24N  -24S    -90N  -64N  -44N  -24N  -EQU  -24S  -44S  -64S
-    # where 24N and -90N should be placed in the same cell (I guess!?).
     for f in files:
         gistemp2csv(f)
